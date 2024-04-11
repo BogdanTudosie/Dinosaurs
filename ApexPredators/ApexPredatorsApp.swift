d079af53beaaca801639789212a09b4dc0d7e74b
Bogdan Tudosie <bogdan.tudosie@withsecure.com>
2024-04-11 16:22:07 +0300

    Initial Commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Bogdan Tudosie on 11.4.2024.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
